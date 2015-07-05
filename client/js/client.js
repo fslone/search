43df5ab9d2985ebf0ab7555fd965611cabef1807
Fleming Slone <fslone@gmail.com>
2015-07-05 12:11:30 -0400

    Adding the refresh page functionality

--- client/js/client.js
+++ client/js/client.js
@@ -12,6 +12,8 @@
 		$form = $("#queryForm");
 		
 		_bindUI();
+
+		_bindRefresh();
 	
 	}
 
@@ -31,7 +33,7 @@
 			$queryBox = $("#query_box");
 			$errorSpan = $(".error-row span");
 
-			query = $queryBox.val();
+			query = $form.serialize();
 
 			if(!query) {
 				$queryBox
@@ -45,13 +47,77 @@
 					.removeClass("has-error");
 				$errorSpan
 					.text("");
-				$.when(_getWikiResults(query)).then(function(wikipediaResults) {
-					_populateWikipediaResults(wikipediaResults);
-				});
+				_showSearchResults();
 			}
 
 		});	
 	
+	}
+
+	function _showSearchResults() {
+		var query = $form.serialize();
+		$.when(
+			_getWikiResults(query), 
+			_getTwitterResults(query)
+		).then(function(wikipediaResults, twitterResults) {
+			_populateWikipediaResults(wikipediaResults);
+			// _populateTwitterResults(wikipediaResults)
+		});
+
+	}
+
+	/**
+    * Re-populate the search box, radius checkbox and search results 
+    * if the page is refreshed.
+    *
+    * @author Fleming Slone [fslone@gmail.com]
+   */
+	function _bindRefresh() {
+
+		var $queryBox, $radius;
+
+		$queryBox = $form.find("#query_box");
+		$radius = $form.find("#radius");
+
+		$(window)
+			.unload(function(){
+
+				var time, queryVal, radius, formState;
+
+				time = new Date().getTime();
+				queryVal = $queryBox.val();
+				radiusVal = $radius.is(":checked");
+				formState = {
+					query: queryVal,
+					radius: radiusVal,
+					unloadTime: time
+				};
+
+				localStorage.setItem("formState", JSON.stringify(formState));
+
+			})
+			.load(function() {
+
+				var curTime, formState, unloadTime;
+				
+				curTime = new Date().getTime();
+				formState = localStorage.getItem("formState");
+				
+				if(formState) {
+
+					formState = $.parseJSON(localStorage.getItem("formState"));
+					unloadTime = formState.unloadTime;
+
+					if((curTime - unloadTime) < 3000) {
+						$queryBox.val(formState.query);
+						$radius.attr("checked", formState.radius);
+						_showSearchResults();
+					}
+
+				}
+
+			});
+
 	}
 
 	/**
@@ -68,16 +134,21 @@
     *
     * @author Fleming Slone [fslone@gmail.com]
    */
-	function _getTwitterResults() {
-		var promise = $.Deferred();
+	function _getTwitterResults(query) {
+
+		var promise, url;
+
+		promise = $.Deferred();
+		url = "/restapi/GetTweets";
 
 		$.ajax({
       cache: false,
+      crossDomain: true,
       type: "GET",
-      url: url,
+      url: url + "?" + query,
       dataType: "text",
       success: function(json) {
-      	promise.resolve($.parseJSON(json))
+      	promise.resolve($.parseJSON(json));
       }
     });
 
@@ -91,19 +162,19 @@
    */
 	function _getWikiResults(query) {
 		
-		var promise, query;
+		var promise, url;
 
 		promise = $.Deferred();
-		query = encodeURIComponent(query);
+		url = "/restapi/GetWikis";
 
     $.ajax({
       cache: false,
       crossDomain: true,
       type: "GET",
-      url: "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" + query,
-      dataType: "jsonp",
+      url: url + "?" + query,
+      dataType: "text",
       success: function(json) {
-      	promise.resolve(json)
+      	promise.resolve($.parseJSON($.parseJSON(json).body));
       }
     });
 
@@ -138,6 +209,46 @@
 		rowClose = "</div>";
 
 		//loop through each search result and append to the tabl
+		$.each(json.query.search, function(i, result) {
+			row = rowOpen;
+			row += titleOpen + result.title + titleClose;
+			row += snippetOpen + result.snippet + "..." + snippetClose;
+			row += rowClose;
+			$wikipedia_results.append(row)
+		});
+
+		$wikipedia_results.slideDown("2000");
+
+	}
+
+	/**
+    * Populate the search engine page with Twitter search results
+    *
+    * @author Fleming Slone [fslone@gmail.com]
+   */
+	function _populateTwitterResults(json) {
+		
+		var $search_container, 
+				rowOpen, 
+				rowClose, 
+				titleOpen, 
+				titleClose, 
+				snippetOpen, 
+				snippetClose, 
+				row; 
+		
+		//cache jQuery reference 		
+		$wikipedia_results = $("#wikipedia_results");
+		$wikipedia_results.hide();
+		$wikipedia_results.empty();
+		rowOpen = "<div class='row'>";
+		titleOpen = "<div class='col-sm-4 col-xs-12'><strong>";
+		titleClose = "</strong></div>";
+		snippetOpen = "<div class='col-sm-8 col-xs-12'>";
+		snippetClose = "</div>";
+		rowClose = "</div>";
+
+		//loop through each search result and append to the table
 		$.each(json.query.search, function(i, result) {
 			row = rowOpen;
 			row += titleOpen + result.title + titleClose;
